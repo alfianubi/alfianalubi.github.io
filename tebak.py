1481e1a994c6a0e744b2a36f77ce33b395b4f8b9
Alfian Al Ayubi <alfianalubi@gmail.com>
2019-04-10 13:43:57 +0700

    update image & favicon

---
+++ tebak.py
@@ -0,0 +1,10 @@
+
+tebakan_tanggal_lahir_gebetan_gue = 1
+tanggal_lahir_gebetan_gue_adalah = 22
+
+while (tebakan_tanggal_lahir_gebetan_gue <= tanggal_lahir_gebetan_gue_adalah):
+    print("Selamat ulang tahun (pada tanggal): ", tebakan_tanggal_lahir_gebetan_gue)
+    tebakan_tanggal_lahir_gebetan_gue = tebakan_tanggal_lahir_gebetan_gue + 1
+
+print ("Akhirnya ketauan!")
+
